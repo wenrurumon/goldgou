47e9f3de5656fb0cbdd3b60a69949455513731f5
進撃の雯雯老師 <askahu6@gmail.com>
2024-09-22 11:38:20 +0800

    Update and rename xunigou_scalelog_b19k2.R to xunigou_scalelog_bk.R

--- goumatrix/xunigou_scalelog_bk.R
+++ goumatrix/xunigou_scalelog_bk.R
@@ -13,6 +13,38 @@ library(randomForest)
 use_condaenv(condaenv='/home/huzixin/anaconda3/envs/test/bin/python',required=TRUE)
 setwd('/home/huzixin/documents/goldgou')
 
+cppFunction('arma::mat corpp(arma::mat X, arma::mat Y) {
+    arma::mat Xt = X.t();
+    arma::mat result = Xt*Y;
+    result /= (Y.n_rows - 1);
+    return result;
+}', depends = "RcppArmadillo")
+
+cppFunction('arma::mat cor_pvalue(arma::mat cor_mat, int n) {
+    arma::mat t_stat = cor_mat % sqrt((n - 2) / (1 - cor_mat % cor_mat));
+    arma::mat p_mat = 2 * arma::normcdf(-arma::abs(t_stat), 0, 1);
+    return p_mat;
+}', depends = "RcppArmadillo")
+
+cppFunction('
+NumericMatrix distCpp(NumericMatrix x1, NumericMatrix x2) {
+  int n1 = x1.nrow();
+  int n2 = x2.nrow();
+  int p = x1.ncol();
+  NumericMatrix dists(n1, n2);
+  for (int i = 0; i < n1; i++) {
+    for (int j = 0; j < n2; j++) {
+      double dist = 0.0;
+      for (int k = 0; k < p; k++) {
+        dist += pow(x1(i, k) - x2(j, k), 2);
+      }
+      dists(i, j) = sqrt(dist);
+    }
+  }
+  
+  return dists;
+}')
+
 ########################################################################################################################
 
 # #Get Historical Data
@@ -43,7 +75,7 @@ setwd('/home/huzixin/documents/goldgou')
 # write.csv(hisdata,'hisdata210101.csv')
 
 #Load Data
-hisdata <- fread('hisdata210101.csv')[,-1]
+hisdata <- fread('hfq180101.csv')[,-1]
 dates <- sort(unique(as.numeric(gsub('-','',unique(hisdata$日期)))))
 
 jgp <- read.csv('raw_grade.csv')[,-1] %>%
@@ -59,7 +91,8 @@ jgp <- read.csv('raw_grade.csv')[,-1] %>%
 rawhis <- hisdata %>% 
   select(date=日期,code,open=开盘,close=收盘,high=最高,low=最低,val=成交额,vol=成交量,name) %>%
   mutate(date=as.numeric(gsub('-','',date))) %>%
-  filter(code%in%jgp$code)
+  filter(code%in%jgp$code) %>%
+  filter(open>0,close>0,low>0,high>0)
 datesi <- sort(unique(rawhis$date))  
 rawhis$did <- match(rawhis$date,datesi)
 codes <- unique(rawhis$code)
@@ -87,7 +120,7 @@ pop <- pop[2:nrow(pop),]-pop[1:(nrow(pop)-1),]
 
 #回测用数据
 
-b <- 19
+b <- 39
 
 system.time(
   datapool <- lapply((b+3):nrow(roi),function(i){
@@ -114,7 +147,8 @@ datapool <- datapool[(which(sapply(datapool,function(x){nrow(x$Y)})>1)[1]+20):le
 #回测
 
 k <- 2
-testperiod <- length(datapool)-(length(datapool)-20):0
+# testperiod <- length(datapool)-200:0
+testperiod <- (k+1):length(datapool)
 
 bench <- rbindlist(
   lapply(datapool[testperiod],function(x){
@@ -128,60 +162,67 @@ bench <- rbindlist(
 
 bench %>%
   group_by('total') %>%
-  summarise(min(obs),max(sell),prod(bench))
+  summarise(min(obs),max(sell),prod(bench,na.rm=T))
 
-system.time(
-  tests <- lapply(testperiod,function(i){
-    print(paste(i,Sys.time()))
-    x.obs <- datapool[[i]]$X
-    y.obs <- datapool[[i]]$Y
-    x.ref <- datapool[[i-k]]$X
-    y.ref <- datapool[[i-k]]$Y
-    do.call(rbind,lapply(1:ncol(x.obs),function(j){
-      xj.dist <- (x.obs[,j]-x.ref)^2
-      n <- nrow(xj.dist) / 2
-      xk <- data.table(
-        code = colnames(x.obs)[j],
-        roidist = colSums(xj.dist[1:n, ]),
-        popdist = colSums(xj.dist[(n + 1):(2 * n), ]),
-        y.ref %>% select(refcode=code,refroi=roi)
-      )
-      xk$roidist <- pnorm(scale(log(xk$roidist)))
-      xk$popdist <- pnorm(scale(log(xk$popdist)))
-      xk <- xk %>%
-        mutate(dist=roidist*popdist)
-      xk
-    })) %>%
-      merge(y.obs) %>%
-      filter(roidist<=0.05,popdist<=0.05) %>%
-      group_by(obs,buy,sell,code) %>%
-      summarise(mean=mean(refroi),win=mean(refroi>1),actual=mean(roi)) %>%
-      filter(mean>1,win>0.5) %>%
-      arrange(desc(mean)) 
-  })
-)
+# system.time(
+#   tests <- lapply(testperiod,function(i){
+#     print(paste(i,length(testperiod)+b,Sys.time()))
+#     x.obs <- datapool[[i]]$X
+#     y.obs <- datapool[[i]]$Y
+#     x.ref <- datapool[[i-k]]$X
+#     y.ref <- datapool[[i-k]]$Y
+#     if(ncol(x.obs)==0){return(NULL)}
+#     do.call(rbind,lapply(1:ncol(x.obs),function(j){
+#       xj.dist <- (x.obs[,j]-x.ref)^2
+#       n <- nrow(xj.dist) / 2
+#       xk <- data.table(
+#         code = colnames(x.obs)[j],
+#         roidist = colSums(xj.dist[1:n, ]),
+#         popdist = colSums(xj.dist[(n + 1):(2 * n), ]),
+#         y.ref %>% select(refcode=code,refroi=roi)
+#       )
+#       xk$roidist <- pnorm(scale(log(xk$roidist)))
+#       xk$popdist <- pnorm(scale(log(xk$popdist)))
+#       xk <- xk %>%
+#         mutate(dist=roidist*popdist)
+#       xk
+#     })) %>%
+#       merge(y.obs) %>%
+#       filter(roidist<=0.01,popdist<=0.01) %>%
+#       group_by(obs,buy,sell,code) %>%
+#       summarise(mean=mean(refroi),win=mean(refroi>1),actual=mean(roi)) %>%
+#       filter(mean>1,win>0.5) %>%
+#       arrange(desc(mean)) 
+#   })
+# )
+# save(tests,file='trace_scalelog_0921b39k2.rda')
+load('trace_scalelog_0921b29k2.rda')
 
-# save(tests,file='trace_scalelog_0921b19k2.rda')
+valid <- rbindlist(lapply(tests,head,5)) %>%
+  group_by(obs) %>%
+  summarise(actual=mean(actual)) %>%
+  group_by(obs=substr(obs,1,6)) %>%
+  summarise(actual=prod((actual-1)/2+1,na.rm=T),n()) %>%
+  merge(
+    bench %>%
+      group_by(obs=substr(obs,1,6)) %>%
+      summarise(bench=prod((bench-1)/2+1))
+  ) 
+valid$cumactual <- cumprod(valid$actual)
+valid$cumbench <- cumprod(valid$bench)
 
-valid <- sapply(5:40,function(i){
-  rbindlist(lapply(tests,head,i)) %>%
-    group_by(obs) %>%
-    summarise(actual=mean(actual)) %>%
-    merge(bench) %>%
-    summarise(prod(actual),prod(bench),mean(actual>bench))
-})
+rlt <- rbindlist(lapply(tests,head,100))
+# write.csv(rlt,'trace_0922slogb19k2.csv')
+
+plot.ts(valid$cumactual,col=2); lines(valid$cumbench)
 
 rbindlist(lapply(tests,head,5)) %>%
   group_by(obs) %>%
   summarise(actual=mean(actual)) %>%
-  group_by(obs=substr(obs,1,6)) %>%
-  summarise(actual=prod(actual,na.rm=T)) %>%
+  group_by(obs=substr(obs,1,1)) %>%
+  summarise(actual=prod((actual-1)/2+1,na.rm=T),n()) %>%
   merge(
     bench %>%
-      group_by(obs=substr(obs,1,6)) %>%
-      summarise(bench=prod(bench))
-  ) %>%
-  melt(id=1) %>%
-  mutate(obs=match(obs,sort(unique(substr(rawhis$date,1,6))))) %>%
-  ggplot() + 
-  geom_line(aes(x=obs,y=value,colour=variable))
+      group_by(obs=substr(obs,1,1)) %>%
+      summarise(bench=prod((bench-1)/2+1))
+  ) 
